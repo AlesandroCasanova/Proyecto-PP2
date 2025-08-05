5f81bc110aa5c2ea9bd198c7f52725c42d5caeee
AlesandroCasanova <alesandrocasanova0112@gmail.com>
2025-08-05 17:57:55 -0300

    Commit de actualizacion
    Agregado de .env, app.js y conexion a la db, el script de la db esta en la carpeta DATABASE, es un script inicial

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,25 @@
-��
+const db = require('./config/db');
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/uploads', express.static('uploads'));
+
+app.get('/api/ping', (req, res) => {
+  res.json({ mensaje: 'Servidor funcionando 🚀' });
+});
+
+const PORT = process.env.PORT || 3000;
+// Probar conexión a la base de datos
+db.getConnection()
+  .then(() => console.log('✅ Conectado a MySQL'))
+  .catch((err) => console.error('❌ Error de conexión a MySQL:', err));
+
+app.listen(PORT, () => {
+  console.log(`✅ Backend corriendo en http://localhost:${PORT}`);
+});
+
